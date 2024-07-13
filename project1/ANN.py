bd4404b5cb11c964d8f709fde9d7c17fc76a01d0
charlesassoi <charlesassoi07@gmail.com>
2024-07-13 13:56:36 +0000

    new commit

---
+++ project1/ANN.py
@@ -0,0 +1,38 @@
+import keras
+
+
+tokenizer = keras_nlp.models.BertTokenizer.from_preset("bert_tiny_en_uncased")
+tokenizer(["I love modular workflows!", "Libraries over frameworks!"])
+
+# Write your own packer or use one of our `Layers`
+packer = keras_nlp.layers.MultiSegmentPacker(
+    start_value=tokenizer.cls_token_id,
+    end_value=tokenizer.sep_token_id,
+    # Note: This cannot be longer than the preset's `sequence_length`, and there
+    # is no check for a custom preprocessor!
+    sequence_length=64,
+)
+
+
+# This function that takes a text sample `x` and its
+# corresponding label `y` as input and converts the
+# text into a format suitable for input into a BERT model.
+def preprocessor(x, y):
+    token_ids, segment_ids = packer(tokenizer(x))
+    x = {
+        "token_ids": token_ids,
+        "segment_ids": segment_ids,
+        "padding_mask": token_ids != 0,
+    }
+    return x, y
+
+
+imdb_train_preprocessed = imdb_train.map(preprocessor, tf.data.AUTOTUNE).prefetch(
+    tf.data.AUTOTUNE
+)
+imdb_test_preprocessed = imdb_test.map(preprocessor, tf.data.AUTOTUNE).prefetch(
+    tf.data.AUTOTUNE
+)
+
+# Preprocessed example
+print(imdb_train_preprocessed.unbatch().take(1).get_single_element())
